dd092e44612ed1c64896f8289af57e588eb91a18
Júlia Rafaela <juliarafaela23101@gmail.com>
2024-12-12 17:01:47 -0300

    veterinario

--- models.py
+++ models.py
@@ -43,11 +43,11 @@ class Animal(Base):
 class Cliente(Base):
     __tablename__ = 'TAB_CLIENTE'
     id_cliente = Column(Integer, primary_key=True)
-    CPF = Column( nullable=False, index=True, unique=True)
-    Nome1 = Column(nullable=False, index=True)
-    telefone = Column(nullable=False, index=True)
-    Profissão2 = Column(nullable=False, index= True)
-    Area2 = Column(nullable=False, index=True)
+    CPF = Column(String(11), nullable=False, index=True, unique=True)
+    Nome1 = Column(String(40), nullable=False, index=True)
+    telefone = Column(String(11), nullable=False, index=True)
+    Profissão2 = Column(String(40), nullable=False, index= True)
+    Area2 = Column(String(40), nullable=False, index=True)
 
     def __repr__(self):
         return '<Atividade: {}'.format(self.nome)
@@ -75,11 +75,14 @@ class Consulta(Base):
     hora = Column(nullable=False, index=True)
     minuto = Column(nullable=False, index=True)
     data1 = Column(nullable=False, index= True)
-    idAnimal2 = Column(nullable=False, index=True)
-    idVeterinario = Column(nullable=False, index=True)
+    valor = Column(nullable=False, index=True)
+    idAnimal2 = Column(Integer, ForeignKey('TAB_ANIMAL.id_animal'))
+    animal = relationship('Animal')
+    idVeterinario = Column(Integer, ForeignKey('TAB_VETERINARIO.id_vet'))
+    veterinario = relationship('Veterinario')
 
     def __repr__(self):
-        return '<Atividade: {}'.format(self.nome)
+        return '<Consulta: {}'.format(self.idConsulta)
 
     def save(self):
         db_session.add(self)
@@ -90,13 +93,71 @@ class Consulta(Base):
         db_session.commit()
 
     def serialize_user(self):
-        dados_atividade = {
-            "id_atividade": self.id,
-            "pessoa_id": self.pessoa_id,
-            "nome": self.nome,
+        dados_consulta = {
+            "idConsulta": self.idConsulta,
+            "motivo_id2": self.motivo_id2,
+            "hora": self.hora,
+            "minuto": self.minuto,
+            "data1'": self.data1,
+            "idAnimal2": self.idAnimal2,
+            "idVeterinario": self.idVeterinario,
         }
-        return dados_atividade
+        return dados_consulta
+class Motivo(Base):
+    __tablename__ = 'TAB_MOTIVO'
+    id_motivo = Column(Integer, primary_key=True)
+    nome_motivo = Column( nullable=False, index=True)
+    motivo_categoria = Column(nullable=False, index=True)
+    valor_motivo = Column(nullable=False, index=True)
 
+    def __repr__(self):
+        return '<Motivo: {}'.format(self.nome_motivo)
+
+    def save(self):
+        db_session.add(self)
+        db_session.commit()
+
+    def delete(self, instance):
+        db_session.delete(self)
+        db_session.commit()
+
+    def serialize_user(self):
+        dados_motivo = {
+            "id_motivo": self.id_motivo,
+            "nome_motivo": self.nome_motivo,
+            "motivo_categoria": self.motivo_categoria,
+            "valor_motivo": self.valor_motivo,
+        }
+        return dados_motivo
+
+class Veterinario(Base):
+    __tablename__ = 'TAB_VETERINARIO'
+    id_vet = Column(Integer, primary_key=True)
+    salario2 = Column( nullable=False, index=True)
+    nomeVet = Column(nullable=False, index=True)
+    crmv = Column(nullable=False, index=True, unique=True)
+    v_consulta1 = Column(nullable=False, index=True)
+
+    def __repr__(self):
+        return '<Veterinario: {}'.format(self.crmv)
+
+    def save(self):
+        db_session.add(self)
+        db_session.commit()
+
+    def delete(self, instance):
+        db_session.delete(self)
+        db_session.commit()
+
+    def serialize_user(self):
+        dados_veterinario = {
+            "id_vet": self.id_vet,
+            "salario2": self.salario2,
+            "nomeVet": self.nomeVet,
+            "crmv": self.crmv,
+            "v_consulta1'": self.v_consulta1,
+        }
+        return dados_veterinario
 
 def init_db():
     Base.metadata.create_all(bind=engine)
